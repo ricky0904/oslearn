3f7d39f2c9371847671b3ee0e3d266e660ba6607
ricky0904 <zxt09041987@gmail.com>
2026-06-24 12:45:37 +0100

    simplifying makefile

--- EnableLong/src/cstart.c
+++ EnableLong/src/cstart.c
@@ -1,10 +1,13 @@
 int cstart()
+
 {
 	//short* a;
 	//volatile short* a = (volatile short *)0xB8000;
 	//short b = *a;
 	volatile unsigned long long *PT; 
+	//unsigned char
 	PT = (volatile unsigned long long *)0x14000;
+	//printchar();
 	//long a = 2;
 	
 	return 0;
---
+++ EnableLong/src/printchar.c
@@ -0,0 +1,6 @@
+int printchar()
+{
+	unsigned char* lookUps = "1234567890ABCDEF";
+	return 0;	
+}
+

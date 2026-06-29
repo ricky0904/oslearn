19869a92b892711cea2586e8c0c2fc5f8411875d
ricky0904 <zxt09041987@gmail.com>
2026-06-29 13:16:17 +0100

    fixing c files and binary sizes, code not tested yet

--- EnableLong/run.sh
+++ EnableLong/run.sh
@@ -1,4 +1,8 @@
 echo making image
 make
-qemu-system-x86_64 -drive file=./build/entry64.bin,format=raw,if=ide -s -S
+qemu-system-x86_64 -drive file=./build/entry64.bin,format=raw,if=ide -smp 2 -s -S
+#		-smp 2, sockets=1, cores=2, threads=1 \
+#		-cpu host \
+#		-accel kvm \
+#		-s -S
 
